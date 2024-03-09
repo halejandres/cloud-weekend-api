c60a95c4cd8d7f369cdbf88defa33d227b0360cb
halejandres <hugo.alejandres@gmail.com>
2024-03-09 07:31:19 -0600

    Response server status added

--- routes/root.js
+++ routes/root.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => { res.send('API para el Cloud Weekend by halejandres') })
+router.get('/', (req, res) => { res.status(200).send('API para el Cloud Weekend by halejandres') })
 
 module.exports = router
